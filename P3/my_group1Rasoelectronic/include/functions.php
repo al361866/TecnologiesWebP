3ef709ba7bd0e009be095e3ff472b41294475884
al361880 <37330374+al361880@users.noreply.github.com>
2019-11-13 17:15:07 +0100

    k

--- P3/my_group1Rasoelectronic/include/functions.php
+++ P3/my_group1Rasoelectronic/include/functions.php
@@ -111,7 +111,7 @@ function MP_my_datos()
             $consult = $MP_pdo->prepare($query);
             $a=$consult->execute($a);
             if (1>$a) {echo "InCorrecto $query";}
-            else wp_redirect(admin_url( 'admin-post.php?action=my_datos&proceso=listar'));
+            //else wp_redirect(admin_url( 'admin-post.php?action=my_datos&proceso=listar'));
             break;
         case "listar":
             //Listado amigos o de todos si se es administrador.
