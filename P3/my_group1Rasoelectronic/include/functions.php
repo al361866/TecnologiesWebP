ac8de71ad670c594bad6e2ca918a04e8f333560e
ferran3998 <37330390+al361866@users.noreply.github.com>
2019-11-13 20:52:02 +0100

    Update functions.php

--- P3/my_group1Rasoelectronic/include/functions.php
+++ P3/my_group1Rasoelectronic/include/functions.php
@@ -33,7 +33,7 @@ function MP_Register_Form($MP_user , $user_email)
 {//formulario registro amigos de $user_email
     ?>
     <h1>Gestión de Usuarios </h1>
-    <form class="fom_usuario" action="?action=my_datosRasoelectronic&proceso=registrar" method="POST" enctype="multipart/form-data">
+    <form class="fom_usuario" action="?action=my_datos&proceso=registrar" method="POST" enctype="multipart/form-data">
         <label for="clienteMail">Tu correo</label>
         <br/>
         <input type="text" name="clienteMail"  size="20" maxlength="25" value="<?php print $user_email?>"
@@ -66,7 +66,7 @@ function MP_Register_Form($MP_user , $user_email)
 //$_REQUEST['proceso'], o sea se activara al llamar a url semejantes a 
 //https://host/wp-admin/admin-post.php?action=my_datos&proceso=r 
 
-function MP_my_datosRasoelectronic()
+function MP_my_datos()
 { 
     global $user_ID , $user_email,$table;
     
@@ -87,7 +87,7 @@ function MP_my_datosRasoelectronic()
     switch ($_REQUEST['proceso']) {
         case "registro":
             $MP_user=null; //variable a rellenar cuando usamos modificar con este formulario
-            MP_Register_FormRasoelectronic($MP_user,$user_email);
+            MP_Register_Form($MP_user,$user_email);
             break;
         case "registrar":
             if (count($_REQUEST) < 4) { //Al ser un parámetro más, incrementamos el num de parámetros
