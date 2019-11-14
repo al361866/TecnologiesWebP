227e7154d2138db37bfca23616ce6f925d59ed63
ferran3998 <37330390+al361866@users.noreply.github.com>
2019-11-14 10:55:39 +0100

    Update my_groupRasoelectronic.php

--- P3/my_group1Rasoelectronic/my_groupRasoelectronic.php
+++ P3/my_group1Rasoelectronic/my_groupRasoelectronic.php
@@ -30,7 +30,7 @@ include(plugin_dir_path( __FILE__ ).'include/functions.php');
 register_activation_hook( __FILE__, 'MP_Ejecutar_crearTRasoelectronic');
 
 //add_action( 'plugins_loaded', 'Ejecutar_crearT' ); // esto se ejecuta siempre que se llama al plugin
-function MP_Ejecutar_crearTRasoelectronic(){
+function MP_Ejecutar_crearT(){
     MP_CrearT("A_GrupoCliente000");
 }
 //add_action('admin_post_nopriv_my_datos', 'MP_my_datos'); //no autentificados
