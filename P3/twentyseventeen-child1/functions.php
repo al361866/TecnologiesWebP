d8318d17e76999a51e2933aef2d2980274f9d381
ferran3998 <37330390+al361866@users.noreply.github.com>
2019-12-04 11:03:01 +0100

    Update functions.php

--- P3/twentyseventeen-child1/functions.php
+++ P3/twentyseventeen-child1/functions.php
@@ -26,7 +26,7 @@ function shortcode_ejercicio8() {
 
 function add_theme_scripts(){
    $deps=array();
-    wp_enqueue_script( 'juego_ej8', get_stylesheet_directory_uri().'/js/ej8.js',$deps,true,true);
+    wp_enqueue_script( 'ej8', get_stylesheet_directory_uri().'/js/ej8.js',$deps,true,true);
 }
 
 add_shortcode('juego_ej8', 'shortcode_ejercicio8');
